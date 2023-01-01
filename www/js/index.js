7fd449c757d2fd433d6e1de58ec6aabafd1a8c37
veetance <divineokos@hotmail.com>
2022-12-31 22:54:11 -0700

    v-page-CTA- is responsive and reusable

--- www/js/index.js
+++ www/js/index.js
@@ -24,12 +24,14 @@ var app = new Framework7({
       url: "index.html",
     },
   ],
+  
+
 });
 var mainView = app.views.create(".view-main");
 
 
+//address bar theme color
 const darkModeQuery = window.matchMedia('not all and (prefers-color-scheme)');
-
 function updateThemeColor(event) {
   if (event.matches) {
     // Dark mode is enabled
@@ -39,8 +41,46 @@ function updateThemeColor(event) {
     document.querySelector('meta[name=theme-color]').setAttribute('content', '#FFFFFF');
   }
 }
-
 darkModeQuery.addEventListener('change', updateThemeColor);
+
+
+
+$(document).ready(function() {
+  // get the button element
+  var vBtni = $('.v-btn-i');
+
+  // add mouseenter and mouseleave event listeners to the button
+  vBtni.mouseenter(function() {
+    // check if the width of the screen is less than 580px
+    if ($(window).width() < 580) {
+      // if the width is less than 580px, do not show the div
+      return;
+    }
+
+    // reveal the div by changing the display to flex and animating the opacity
+    $('.v-bottom').css({
+      'display': 'flex',
+      'opacity': 0
+    }).animate({
+      'opacity': 1
+    }, 200);
+  });
+  vBtni.mouseleave(function() {
+    // check if the width of the screen is less than 580px
+    if ($(window).width() < 580) {
+      // if the width is less than 580px, do not hide the div
+      return;
+    }
+
+    // hide the div by animating the opacity to 0
+    $('.v-bottom').animate({
+      'opacity': 0
+    }, 200, function() {
+      // after the animation, change the display back to none
+      $(this).css('display', 'none');
+    });
+  });
+});
 
 
 
@@ -79,3 +119,5 @@ function navTitle() {
 }
 window.addEventListener("resize", navTitle);
 navTitle();
+
+
