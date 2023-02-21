40703d2299540f28ae102252ac024028722f86ec
veetance <divine.okonkwo@uleth.ca>
2023-02-21 15:47:14 -0700

    making login page -responsive-02

--- js/index.js
+++ js/index.js
@@ -13,44 +13,6 @@ function updateThemeColor(event) {
 darkModeQuery.addEventListener('change', updateThemeColor);
 
 
-//reveal animation
-$(document).ready(function() {
-  // get the button element
-  var vBtni = $('.v-btn-i');
-
-  // add mouseenter and mouseleave event listeners to the button
-  vBtni.mouseenter(function() {
-    // check if the width of the screen is less than 580px
-    if ($(window).width() < 580) {
-      // if the width is less than 580px, do not show the div
-      return;
-    }
-
-    // reveal the div by changing the display to flex and animating the opacity
-    $('#').css({
-      'display': 'flex',
-      'opacity': 0
-    }).animate({
-      'opacity': 1
-    }, 200);
-  });
-  vBtni.mouseleave(function() {
-    // check if the width of the screen is less than 580px
-    if ($(window).width() < 580) {
-      // if the width is less than 580px, do not hide the div
-      return;
-    }
-
-    // hide the div by animating the opacity to 0
-    $('#').animate({
-      'opacity': 0
-    }, 200, function() {
-      // after the animation, change the display back to none
-      $(this).css('display', 'none');
-    });
-  });
-});
-
 
 // on first page load function splash() pulses .v-logo-wrapper starting from opacity 0 to opacity 1 ease in and 2 times in 1.5s and then fades out while fading out .v-splash and setting display to none
 
