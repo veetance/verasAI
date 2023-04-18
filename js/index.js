66e3d549394967eeabe365b7c9b934e519277e22
veetance <divine.okonkwo@uleth.ca>
2023-04-18 04:19:33 -0600

    SWAP-UI-Figma Update Newsfeed layout

--- js/index.js
+++ js/index.js
@@ -96,138 +96,147 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
   }
-
-  // Store subscription to handle state changes
+  
   store.subscribe(() => {
     const state = store.getState();
-
+  
     // If the login form should be visible
     if (state.loginVisible) {
       let loginSpace = document.querySelector(".login-Space");
-
+  
       if (!loginSpace) {
         loginSpace = document.createElement("div");
         loginSpace.classList.add("login-Space", "fade-in");
         surfaceView.insertBefore(loginSpace, surfaceView.firstChild);
       }
-
+  
       loginSpace.innerHTML = state.loginContent;
       document.title = "Login";
       surfaceView.style.opacity = 0;
       surfaceView.innerHTML = "";
       surfaceView.appendChild(loginSpace);
       footer.style.display = "none";
-
+  
       setTimeout(() => {
         loginSpace.classList.add("active");
         surfaceView.style.opacity = 1;
       }, 100);
-
+  
       // Add event listener for form submission, only if it hasn't been added before
       if (!loginSpace.dataset.formEventAttached) {
-        document.addEventListener("DOMContentLoaded", () => {
-          // Your entire script here
-        });
-
+     
+  
         loginSpace.dataset.formEventAttached = "true";
-
+  
         setTimeout(() => {
           const loginForm = document.querySelector(".form");
           loginForm.addEventListener("submit", (event) => {
             event.preventDefault();
-
+  
             const loginNumberInput = document.querySelector("#login-number");
             const passwordInput = document.querySelector("#password");
-
+  
             // Fetch the newsfeed.html content and set it to the state
             fetch("pages/newsfeed.html")
-            .then((response) => response.text())
-            .then((html) => {
-              store.dispatch(setNewsfeedContent(html));
-              window.location.href = "pages/newsfeed.html";
-            });
-
+              .then((response) => response.text())
+              .then((html) => {
+                store.dispatch(setNewsfeedContent(html));
+                window.location.href = "pages/newsfeed.html";
+              });
+  
             if (loginNumberInput.value === "1" && passwordInput.value === "1") {
               store.dispatch(hideLogin());
               store.dispatch(showNewsfeed());
             }
-
           });
         }, 100);
       }
-
-      
+  
     } else if (state.newsfeedVisible) {
       // If the newsfeed should be visible
       let newsfeedSpace = document.querySelector(".newsfeed-Space");
-
+  
       if (!newsfeedSpace) {
         newsfeedSpace = document.createElement("div");
         newsfeedSpace.classList.add("newsfeed-Space", "fade-in");
         surfaceView.insertBefore(newsfeedSpace, surfaceView.firstChild);
       }
-
+  
       newsfeedSpace.innerHTML = state.newsfeedContent;
       document.title = "Newsfeed";
       surfaceView.style.opacity = 0;
       surfaceView.innerHTML = "";
       surfaceView.appendChild(newsfeedSpace);
       footer.style.display = "block";
-
+  
       setTimeout(() => {
         newsfeedSpace.classList.add("active");
         surfaceView.style.opacity = 1;
       }, 100);
-    } else if (state.insightsVisible) {
-      let insightsSpace = document.querySelector(".insights-Space");
-
-      if (!insightsSpace) {
-        insightsSpace = document.createElement("div");
-        insightsSpace.classList.add("insights-Space");
-        feedScroll.appendChild(insightsSpace);
-
-        fetch("../pages/insights.html")
-          .then((response) => response.text())
-          .then((html) => {
-            insightsSpace.innerHTML = html;
-          });
-      }
-
-      feedWrapper.style.display = "none";
-    } else if (state.createVisible) {
-      let createSpace = document.querySelector(".create-Space");
-
-      if (!createSpace) {
-        createSpace = document.createElement("div");
-        createSpace.classList.add("create-Space");
-        feedScroll.appendChild(createSpace);
-
-        fetch("../pages/create.html")
+  
+    } else {
+      // insights and create page
+      if (state.insightsVisible) {
+        let insightsSpace = document.querySelector(".insights-Space");
+  
+        if (!insightsSpace) {
+          insightsSpace = document.createElement("div");
+          insightsSpace.classList.add("insights-Space");
+          feedScroll.appendChild(insightsSpace);
+  
+          fetch("../pages/insights.html")
+            .then((response) => response.text())
+            .then((html) => {
+              insightsSpace.innerHTML = html;
+            });
+        }
+  
+        let createSpace = document.querySelector(".create-Space");
+        if (createSpace) {
+          createSpace.style.display = "none";
+        }
+  
+        feedWrapper.style.display = "none";
+  
+      } else if (state.createVisible) {
+        let createSpace = document.querySelector(".create-Space");
+  
+        if (!createSpace) {
+          createSpace = document.createElement("div");
+          createSpace.classList.add("create-Space");
+          feedScroll.appendChild(createSpace);
+          fetch("../pages/create.html")
           .then((response) => response.text())
           .then((html) => {
             createSpace.innerHTML = html;
           });
       }
+
+      let insightsSpace = document.querySelector(".insights-Space");
+      if (insightsSpace) {
+        insightsSpace.style.display = "none";
+      }
       feedWrapper.style.display = "none";
+
     } else {
       document.addEventListener("DOMContentLoaded", () => {
         // Your entire script here
         feedWrapper.style.display = "flex";
 
-        const insightsSpace = document.querySelector(".insights-Space");
         if (insightsSpace) {
           insightsSpace.style.display = "none";
         }
 
-        const createSpace = document.querySelector(".create-Space");
         if (createSpace) {
           createSpace.style.display = "none";
         }
+
+        feedWrapper.style.display = "flex";
       });
     }
-  });
+  }
+});
 
-  // Event listener to handle browser navigation
   window.addEventListener("popstate", () => {
     const currentPage = window.location.pathname.replace("/", "");
     store.dispatch(setCurrentPage(currentPage));
